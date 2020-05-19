edae50c1dce82c53c98e5a5efea80ccf2e70978c
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-19 14:57:53 +0200

    failing cucumber test
    - renamed package

---
+++ src/test/java/no/nav/bidrag/test/gh/actions/BidragTestGhActionsApplicationTests.java
@@ -0,0 +1,12 @@
+package no.nav.bidrag.test.gh.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidragTestGhActionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
---
+++ src/test/kotlin/no/nav/bidrag/test/gh/actions/cucumber/RunCucumberTest.kt
@@ -0,0 +1,12 @@
+package no.nav.bidrag.test.gh.actions.cucumber
+
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+/**
+ * Runner class which looks for feature-files (in src/test/resources/no.nav.bidrag.cucumber.*) to test
+ */
+@RunWith(Cucumber::class)
+@CucumberOptions(plugin = ["pretty", "json:target/cucumber-report/cucumber.json"])
+class RunCucumberTest
